6a8f890783d8c2d1c22bcbf2d5857aaf4b3b5168
plaus <plaus@live.unc.edu>
2015-01-29 08:32:24 -0500

    the stuff

---
+++ script.js
@@ -0,0 +1,19 @@
+//this is a javascript file
+/*
+var userName = "Matt";
+
+var todayDate= new Date();
+
+function myFunction(){
+    document.write("Write this");
+    document.write(userName);
+    console.log(todayDate);
+};
+
+
+var myNumberVariable = 5;
+var myTextVariable = "My variable is a String";
+var myDateVariable= new Date();
+console.log(myNumberVariable);
+console.log(myTextVariable);
+myFunction();*/
